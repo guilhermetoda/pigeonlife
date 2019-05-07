cd9af64c68816a8f14c0a1051e2706b5a362180f
Guilherme Alves Toda <gd53guilherme@vfs.local>
2019-05-07 16:23:22 -0700

    proto start

---
+++ Assets/Scripts/Flying.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Flying : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            _rb.AddForce(Vector3.up*10f);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            _rb.AddForce(Vector3.forward * 10f);
+        }
+    }
+}
