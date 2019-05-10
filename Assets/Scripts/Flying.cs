e930f69e3b9f74ab77dac0cba8d5291c29aa396c
Guilherme Alves Toda <gd53guilherme@vfs.local>
2019-05-10 16:57:45 -0700

    Second Proto test

--- Assets/Scripts/Flying.cs
+++ Assets/Scripts/Flying.cs
@@ -6,20 +6,71 @@ using UnityEngine;
 public class Flying : MonoBehaviour
 {
     private Rigidbody _rb;
+    
+    private float _flyForce = 10f;
+    private float _flyForwardForce = 10f;
+
+    private float _xInput = 0f; // X-Axis Input 
+    private float _yInput = 0f; // Y-Axis Input 
+
+    private float _flySpeed = 1f;
+
+    private bool _flyPressed = false;
+    private bool _flyHold = false;
+
+    [SerializeField] private Camera _birdCamera;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W)) 
+        _xInput = Input.GetAxis("Vertical");
+        _yInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            _rb.AddForce(Vector3.up*10f);
+           _flyPressed = true;
         }
 
-        if (Input.GetKey(KeyCode.UpArrow)) 
+        if (Input.GetKeyUp(KeyCode.Space)) 
         {
-            _rb.AddForce(Vector3.forward * 10f);
+           _flyPressed = false;
+           _flyHold = false;
         }
+
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            _flyHold = true;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (_flyPressed) 
+        {   
+            _rb.AddForce(Vector3.up * 15f);
+            /* var newVelocity = new Vector3(0f , 0f, 1f).normalized * _flySpeed;
+            newVelocity = transform.TransformVector(newVelocity);
+            _rb.velocity = newVelocity;*/
+        }
+        else if (_flyHold) 
+        {   
+            _rb.AddForce(Vector3.up * 5f);
+            /* var newVelocity = new Vector3(0f , 0f, 1f).normalized * _flySpeed;
+            newVelocity = transform.TransformVector(newVelocity);
+            _rb.velocity = newVelocity;*/
+        }
+
+        _rb.AddRelativeForce(transform.forward*_xInput*5f);
+
+        //transform.Rotate(new Vector3(_xInput, 0f, 0f));
+        
+        //_birdCamera.transform.Rotate(new Vector3(0f,_yInput,0f));
+        transform.Rotate(new Vector3(0f,_yInput,0f));
     }
 }
